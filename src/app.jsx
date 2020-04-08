35513b12ce68104d40b36f533bbd0a31cce15c8c
David McGinnis <656337+davidov541@users.noreply.github.com>
2020-04-08 15:28:36 -0500

    Adding a few tools to make backwards compatibility and dev easier.

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,9 @@
 const continents = ['North America', 'South America', 'Africa', 'Asia', 'Europe', 'Australia', 'Antartica']
+const helloContinents = Array.from(continents, c => `Hello ${c}!`)
+const message = helloContinents.join('\n')
 const element = (
     <div title="Outer div">
-        <h1>Hello World!</h1>
+        <h1>{message}</h1>
     </div>
 )
 ReactDOM.render(element, document.getElementById('content'))
